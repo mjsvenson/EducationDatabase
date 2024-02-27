11cdb6e77eaf48c0e42fce2a1b77de04633270ce
Matthew Svenson <matthewjsvenson@gmail.com>
2024-02-27 16:52:35 -0500

    figured out how to run it

--- Education.php
+++ Education.php
@@ -11,26 +11,5 @@ $dbname = "db2";
 
 //establish connection using info
 $conn = new mysqli($servername, 'root', ' ');
-    or die ('Could not connect: ' . mysql_error());
-    
-//check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-//prepare insert statement
-$stmt = $conn->prepare("INSERT INTO student (email, password) VALUES (?, ?)");
-$stmt->bind_param("ss", $email, $password);
-
-//execute and check
-if ($stmt->execute()) {
-    echo "Registration successful!";
-} else {
-    echo "Error: " . $stmt->error;
-}
-
-//close connection
-$stmt->close();
-$conn->close();
-
+ 
 ?>
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EducationDatabase
 An example of SQL knowledge using a university database
+
+To run this code:
+1. Make sure the correct files are in the XAMPP file directory (something like):
+C:\xampp\htdocs\ whatever files you want to run here
+
+2. Run the files by typing this in your browser:
+localhost/[filename]
