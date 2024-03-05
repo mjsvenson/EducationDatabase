b368dc0a47a067d258f0732cebd7d452b1d6cdbc
Matthew Svenson <matthewjsvenson@gmail.com>
2024-03-04 20:34:29 -0500

    Update SignIn.php
    Sign In now works correctly, need to add a signed in webpage

---
+++ SignIn.php
@@ -0,0 +1,44 @@
+<?php
+//validate data
+if (!isset($_POST['Email']) || !isset($_POST['password'])) {
+    echo "Username and password are required.";
+    exit;
+}
+
+//take email and password
+$email = $_POST['Email'];
+$password = $_POST['password'];
+
+//enter server info
+$servername = "localhost";
+$dbname = "db2";
+$student = "student";
+
+//establish connection using info
+$conn = new mysqli($servername, 'root', '', $dbname);
+
+//check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//prepare insert statement
+$sql = "SELECT * FROM account WHERE email = '$email' AND password = '$password'";
+$result = $conn->query($sql);
+
+$SignIn_page = "SignIn.html";
+
+//execute and check
+if ($result->num_rows > 0) {
+    // Credentials found, do something
+    echo "Credentials found!";
+} else {
+    // No match found for the given credentials
+    echo "Invalid credentials!";
+}
+
+//close connection
+$conn->close();
+
+ 
+?>
