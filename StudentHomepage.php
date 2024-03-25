f99c000c2664ed669037bc00d1e0ddffd8df8fbb
Matthew Svenson <matthewjsvenson@gmail.com>
2024-03-25 16:26:25 -0400

    Fixed homepage look

--- StudentHomepage.php
+++ StudentHomepage.php
@@ -13,6 +13,7 @@ $name = $_SESSION['Email'];
 <body>
     <h1>University Website</h1>
     <h2>Student Homepage</h2>
+    <?php echo "Hello $name";?><br></br>
     <a href="ModifyInfo.php"><button>Modify Information</button></a>
     <p></p>
     <a href="CreateStudyGroup.php"><button>Create a Study Group</button></a>
